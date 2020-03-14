86f3504fa1e5f87603b5fcc69d7e1571e553330e
Jia Liu <jialiu@login1.stampede2.tacc.utexas.edu>
2020-03-14 13:14:26 -0500

    update compression script

--- file_compression.sh
+++ file_compression.sh
@@ -1,6 +1,6 @@
-scp ~/scratch/Outlier/cls/clkk_000999.npy dl:~/www/columbialensing/outlier
-scp ~/scratch/Outlier/GRFs/GRF_000999.fits dl:~/www/columbialensing/outlier
+#scp ~/scratch/Outlier/cls/clkk_000999.npy dl:~/www/columbialensing/outlier
+#scp ~/scratch/Outlier/GRFs/GRF_000999.fits dl:~/www/columbialensing/outlier
 
-tar -cf ~/scratch/Outlier/GRFs.tar ~/scratch/Outlier/GRFs
+tar cvf ~/scratch/Outlier/GRFs.tar ~/scratch/Outlier/GRFs
 scp ~/scratch/Outlier/GRFs.tar dl:~/www/columbialensing/outlier/
 scp ~/work/Outlier/GRF_params_output.txt dl:~/www/columbialensing/outlier/
