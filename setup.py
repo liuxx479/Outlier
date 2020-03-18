eeecd10cca478401905beef55e618053b08a9687
EiffL <fr.eiffel@gmail.com>
2020-03-18 19:00:57 +0100

    Addes structure for dataset

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Outlier',
+    version='0.1.0',
+    url='https://github.com/liuxx479/Outlier.git',
+    author='Jia Liu, Vanessa Boehm, Francois Lanusse',
+    description='Code for outlier detection in numerical simulations',
+    packages=find_packages(),
+    install_requires=['tensorflow_datasets', 'tensorflow'],
+)
